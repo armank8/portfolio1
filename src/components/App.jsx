b08851f27b358d6c26a01292190cba930e34a39a
Arman-Kabir <armankabir8@gmail.com>
2024-04-29 16:11:13 +0600

    about alignment done

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import "../App.css";
+import About from "./About";
+
+function App() {
+  return (
+    <div style={{ marginTop: "5rem" }}>
+      <About></About>
+    </div>
+  );
+}
+
+export default App;
