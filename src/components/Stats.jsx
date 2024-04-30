19e81697b1ebd17bc9b40a7c91a4d9252f009591
Arman-Kabir <armankabir8@gmail.com>
2024-04-30 18:01:07 +0600

    stats done

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,5 +1,43 @@
 export default function Stats() {
-  return <section className="stats" id="stats">
-    stats
-  </section>;
+  return (
+    <section className="stats" id="stats">
+      <div className="section_container">
+        <div className="stats_header">
+          <h2>Stats</h2>
+          <p>Projects I have completed</p>
+        </div>
+
+        <div className="stats_container">
+          <div className="stat_box">
+            <h3>React</h3>
+            <p>10+</p>
+          </div>
+          <div className="stat_box">
+            <h3>Node</h3>
+            <p>10+</p>
+          </div>
+          <div className="stat_box">
+            <h3>Next</h3>
+            <p>4</p>
+          </div>
+          <div className="stat_box">
+            <h3>MongoDB</h3>
+            <p>10+</p>
+          </div>
+          <div className="stat_box">
+            <h3>postgreSQL</h3>
+            <p>3</p>
+          </div>
+        </div>
+        <div className="github">
+          <h2>
+            See complete reference to <br></br>
+            <a href="https://github.com/armank8" target="_blank">
+              Github
+            </a>
+          </h2>
+        </div>
+      </div>
+    </section>
+  );
 }
