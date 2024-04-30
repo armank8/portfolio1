a805d298eb5c6940953f7fdeab7f3edf4a441f83
Arman-Kabir <armankabir8@gmail.com>
2024-04-30 08:05:55 +0600

    education ewu part done

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -9,13 +9,15 @@ export default function Education() {
         <div className="education_container">
           {/* Ewu */}
           <div className="ewu">
-            <div>
-              <p>B.Sc in Computer Science and Engineering(CSE)</p>
-              <p>East West University, Bangladesh</p>
-              <p>2014-2020</p>
-              <p>73% number</p>
+            <div className="ewu_info">
+              <div  className="ewu_details">
+                <p>B.Sc in Computer Science and Engineering(CSE)</p>
+                <p>East West University, Bangladesh</p>
+                <p>2014-2020</p>
+                <p>73% number</p>
+              </div>
             </div>
-            <div>
+            <div className="ewu-img">
               <img src={arman3} alt="" />
             </div>
           </div>
