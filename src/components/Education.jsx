c74ba00d56eb95d6291d90e4d373efa6355a85e5
Arman-Kabir <armankabir8@gmail.com>
2024-04-30 10:24:33 +0600

    institution level1 done level2 half done react-pdf added

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,4 +1,6 @@
 import arman3 from "../assets/arman3.jpg";
+import certificate1 from "../assets/certificate1.jpg";
+import certificate2 from "../assets/certificate2.pdf";
 
 export default function Education() {
   return (
@@ -8,17 +10,54 @@ export default function Education() {
 
         <div className="education_container">
           {/* Ewu */}
-          <div className="ewu">
-            <div className="ewu_info">
-              <div  className="ewu_details">
+          <div className="institution">
+            <div className="institution_info">
+              <div className="institution_details">
                 <p>B.Sc in Computer Science and Engineering(CSE)</p>
                 <p>East West University, Bangladesh</p>
                 <p>2014-2020</p>
                 <p>73% number</p>
               </div>
             </div>
-            <div className="ewu-img">
+            <div className="institution_img">
               <img src={arman3} alt="" />
+            </div>
+          </div>
+
+          {/* Level-1 */}
+          <div className="institution">
+            <div className="institution_info">
+              <div className="institution_details">
+                <p>B.Sc in Computer Science and Engineering(CSE)</p>
+                <p>East West University, Bangladesh</p>
+                <p>2014-2020</p>
+                <p>73% number</p>
+              </div>
+            </div>
+            <div className="institution_img">
+              <img src={certificate1} alt="" />
+            </div>
+          </div>
+
+          {/* Level2 */}
+          <div className="institution">
+            <div className="institution_info">
+              <div className="institution_details">
+                <p>B.Sc in Computer Science and Engineering(CSE)</p>
+                <p>East West University, Bangladesh</p>
+                <p>2014-2020</p>
+                <p>73% number</p>
+              </div>
+            </div>
+            <div className="institution_img">
+              {/* <iframe
+                src="https://drive.google.com/file/d/1GkLi7S9Qf2QgAfdxfVMA8DbDHqiGomg1/preview"
+                width='100%'
+                height={400}
+              ></iframe> */}
+              <object data="https://drive.google.com/file/d/1GkLi7S9Qf2QgAfdxfVMA8DbDHqiGomg1/preview" width="50rem">
+
+              </object>
             </div>
           </div>
         </div>
