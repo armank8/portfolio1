c0fb06a8d832a1c2e2b59dc7c63a07125ecd2d11
Arman-Kabir <armankabir8@gmail.com>
2024-05-01 11:55:17 +0600

    skills data and icons created

---
+++ src/data/skills.js
@@ -0,0 +1,66 @@
+import { FaReact } from "react-icons/fa";
+import { BiLogoRedux } from "react-icons/bi";
+import { TbBrandNextjs } from "react-icons/tb";
+import { BiLogoTailwindCss } from "react-icons/bi";
+import { IoLogoCss3 } from "react-icons/io";
+import { FaNode } from "react-icons/fa";
+import { SiExpress } from "react-icons/si";
+import { BiLogoMongodb } from "react-icons/bi";
+import { SiMongoose } from "react-icons/si";
+import { TbFileTypeSql } from "react-icons/tb";
+import { DiPostgresql } from "react-icons/di";
+import { SiPrisma } from "react-icons/si";
+
+export const frontEndSkills=[
+    {
+        text:"React",
+        icon: <FaReact />,
+    },
+    {
+        text:"Redux",
+        icon: <BiLogoRedux />,
+    },
+    {
+        text:"Next",
+        icon: <TbBrandNextjs />,
+    },
+    {
+        text:"TailwindCss",
+        icon: <BiLogoTailwindCss />,
+    },
+    {
+        text:"Css",
+        icon: <IoLogoCss3 />,
+    }
+]
+
+export const backEndSkills=[
+    {
+        text:"Node",
+        icon: <FaNode />,
+    },
+    {
+        text:"Express",
+        icon: <SiExpress />,
+    },
+    {
+        text:"MongoDB",
+        icon: <BiLogoMongodb />,
+    },
+    {
+        text:"Mongoose",
+        icon: <SiMongoose />,
+    },
+    {
+        text:"SQL",
+        icon: <TbFileTypeSql />,
+    },
+    {
+        text:"PostGreSQL",
+        icon: <DiPostgresql />,
+    },
+    {
+        text:"Prisma",
+        icon: <SiPrisma />,
+    }
+]
