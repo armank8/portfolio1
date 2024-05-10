452fc24be788fc9b2da95b0385fe5104c41ecb67
Arman-Kabir <armankabir8@gmail.com>
2024-05-10 12:52:02 +0600

    about section redesigned

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,38 +1,47 @@
+import arman from "../assets/arman.png";
+import styles from "../styles/About.module.css";
+
 export default function About() {
   return (
     <section className="about" id="about">
       <div className="section_container">
-        <div className="about_container">
-          <h2>About</h2>
-          <p>
-            I am an enthusiastic and skilled web developer with a deep passion
-            for creating outstanding web applications. I have a degree in
-            computer science and engineering and participated 1 year long
-            professional web development bootcamp in which i scored top 5%
-            performance. My expertise expands entire web development stack from
-            the front-end to back-end and i love working with a diverse set of
-            technologies to create dynamic and user-friendly web applications.
-          </p>
-          <p>
-            In the front-end , my proficiency includes HTML, CSS, javaScript,
-            TypeScript, ReactJs, NextJs and Redux. I'm well versed in
-            component-driven development and i enjoy working with UI libraries
-            such as Tailwind CSS, Shadcn UI, Daisy UI, Ant Design, Bootstrap. I
-            manage data and tate with React Query and RTK Query. I utilize tools
-            like Axios, React-Hook-Form and Yp validation to make interaction
-            both engaging and secure. For seamless user-authentication i use
-            Firebase, NextAuth and custom backend-based authentication if
-            necessary. For quick e-commerce solution - I use wordpress,
-            woo-commerce and elementor.
-          </p>
-          <p>
-            In the back-end, m expertise are NodeJs,ExpressJs, MongoDB, SQL,
-            PostGreSQL, Mongoose, Prisma. I use both SQL and NoSQL based
-            databases in my projects. I write backend api i MVC pattern or
-            modular pattern. I use Zod for data validation and JWT for
-            authorization and authentication.Feel free to connect with me. I am
-            eager to collaborate on exciting projects.
-          </p>
+        <div className={styles.about_container}>
+          <div className={styles.about_image}>
+            <h2>About Me</h2>
+            <img style={{ width: "80%" }} src={arman} alt="" />
+          </div>
+          <div className={styles.para_container}>
+            <p className="para">
+              I am an enthusiastic and skilled web developer with a deep passion
+              for creating outstanding web applications. I have a degree in
+              computer science and engineering and participated 1 year long
+              professional web development bootcamp in which i scored top 5%
+              performance. My expertise expands entire web development stack
+              from the front-end to back-end and i love working with a diverse
+              set of technologies to create dynamic and user-friendly web
+              applications.
+            </p>
+            <p className="para">
+              In the front-end , my proficiency includes HTML, CSS, javaScript,
+              TypeScript, ReactJs, NextJs and Redux. I'm well versed in
+              component-driven development and i enjoy working with UI libraries
+              such as Tailwind CSS, Shadcn UI, Daisy UI, Ant Design, Bootstrap.
+              I manage data and tate with React Query and RTK Query. I utilize
+              tools like Axios, React-Hook-Form and Yp validation to make
+              interaction both engaging and secure. For seamless
+              user-authentication i use Firebase, NextAuth and custom
+              backend-based authentication if necessary. For quick e-commerce
+              solution - I use wordpress, woo-commerce and elementor.
+            </p>
+            <p className="para">
+              In the back-end, m expertise are NodeJs,ExpressJs, MongoDB, SQL,
+              PostGreSQL, Mongoose, Prisma. I use both SQL and NoSQL based
+              databases in my projects. I write backend api i MVC pattern or
+              modular pattern. I use Zod for data validation and JWT for
+              authorization and authentication.Feel free to connect with me. I
+              am eager to collaborate on exciting projects.
+            </p>
+          </div>
         </div>
       </div>
     </section>
